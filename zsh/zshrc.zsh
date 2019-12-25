87692b52a83f8267fb97a8287d36360a63c5db8e
Jeff Bromen <jabromen1@gmail.com>
2019-12-25 15:52:17 -0600

    add ll alias

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -19,6 +19,11 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+# shorter list view of ls
+alias ll='ls -Al'
+
+alias ~='cd ~'
+
 # print directory contents when entering it
 chpwd() ls
 
