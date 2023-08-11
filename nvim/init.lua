ec6827ef4fcfbb2f4b2e585f195664b7273c3856
Jeff Bromen <jabromen1@gmail.com>
2023-08-11 10:35:07 -0500

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,6 +35,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- Auto close braces
+  'jiangmiao/auto-pairs',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
